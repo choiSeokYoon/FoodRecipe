43c03c1503ec4d4cfe2bb19bee43c4194e3cfdad
WKWKA <ctd0606@naver.com>
2023-03-06 17:38:05 +0900

    banner

--- src/components/ranking/RankingPost.jsx
+++ src/components/ranking/RankingPost.jsx
@@ -1,52 +1,48 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import { useRecoilValue } from 'recoil';
 import { fetchPostData } from '../../recoil/selector';
-import './RankingPost.scss'
-
+import './RankingPost.scss';
 
 export default function RankingPost() {
-    const posts = useRecoilValue(fetchPostData)
+  const posts = useRecoilValue(fetchPostData)
     const dietFood = [...posts];
     dietFood.sort((a, b) => a.INFO_ENG - b.INFO_ENG);
     console.log(dietFood);
     
   return (
-    (dietFood && 
-    <div className='ranking_posts'>
-      <div className='container'>
-          <div className='title'><h1>랭킹모음</h1></div>
-      </div>
-        <div className='ranking_box'>
-          <ul className='ranking_table'>
-            {dietFood.map((item, idx)=>(
-              <li className='ranking_list'key={item.id}>
-                <p className='num'>{idx+1}</p>
-                <div className='ranking_img'>
+    dietFood && (
+      <div className="ranking_posts">
+        <div className="container">
+          <div className="title">
+            <h1>랭킹모음</h1>
+          </div>
+        </div>
+        <div className="ranking_box">
+          
+          <ul className="ranking_table">
+            <li className='ranking_table_top'>
+              <p className='num'>num</p>
+              <p className="ranking_img">이미지</p>
+              <p className="ranking_title">음식 레시피</p>
+              <p className="food_type">칼로리</p>
+              <p className="food_type">카테고리</p>
+              <p className="food_tag">태그</p>
+            </li>
+            {dietFood.map((item, idx) => (
+              <li className="ranking_list" key={item.id}>
+                <p className="num">{idx + 1}</p>
+                <div className="ranking_img">
                   <img src={item.ATT_FILE_NO_MAIN} alt="" />
                 </div>
-                <div className='ranking_title'>{item.RCP_NM}</div>
-                <div className='ranking_nutriment'>{item.INFO_ENG}</div>
-                <div className='food_type'>{item.RCP_PAT2}</div>
-                <div className='food_tag'>{item.HASH_TAG}</div>
+                <div className="ranking_title">{item.RCP_NM}</div>
+                <div className="ranking_nutriment">{item.INFO_ENG}</div>
+                <div className="food_type">{item.RCP_PAT2}</div>
+                <div className="food_tag">{item.HASH_TAG}</div>
               </li>
-              
             ))}
-            
-            
-          </ul>
-          <ul className='ranking_category'>
-              <li>열량</li>
-              <li>탄수화물</li>
-              <li>단백질</li>
-              <li>지방</li>
-              <li>나트륨</li>
           </ul>
         </div>
-        
-        
-    </div>
+      </div>
     )
- )
-  
+  );
 }
-
--- src/container/Banner.jsx
+++ src/container/Banner.jsx
@@ -4,46 +4,57 @@ import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
 import { Pagination, Navigation, Autoplay, EffectFade } from "swiper";
-import bannerImg1 from '../assets/main_1.jpg'
-import bannerImg2 from '../assets/main_2.jpg'
+import bannerImg7 from '../assets/main7.jpg'
+import bannerImg8 from '../assets/main8.jpg'
+import bannerImg3 from '../assets/main3.jpg'
+import bannerImg4 from '../assets/main4.jpg'
+import bannerImg5 from '../assets/main5.jpg'
+import bannerImg6 from '../assets/main6.jpg'
 
 export default function Banner() {
   
   return (
     <div className='banner'>
-      <div className='slide'>
-        <Swiper
-          modules={[Navigation, Pagination, Autoplay, EffectFade]}
-          slidesPerView={1}
-          spaceBetween={30}
-          loop={true}
-          pagination={{
-            clickable: true,
-            el:".swiper_pagination"
-            
-          }}
-          autoplay={{ 
-            delay: 3000,
-            disableOnInteraction: false,
-          }}
-          navigation={true}
-          className="slide_box"
-        >
-          <SwiperSlide className='swiperSlide'>
-            <img src={bannerImg1} alt="" />
-            <div className="slide_text">
-              <p>한끼에 담은 오늘의 </p>
-            </div>
-          </SwiperSlide>
-          <SwiperSlide className='swiperSlide'>
-            <img src={bannerImg2} alt="" />
-            <div className="slide_text" >
-              <p>가장 풍요롭고 생생한 시간</p>
-            </div>
-          </SwiperSlide>
-          <div className='swiper_pagination'></div>
-        </Swiper>
-      </div>
+      
+        <div className='slide'>
+          <Swiper
+            modules={[Navigation, Pagination, Autoplay, EffectFade]}
+            slidesPerView={1}
+            spaceBetween={30}
+            loop={true}
+            pagination={{
+              clickable: true,
+              el:".swiper_pagination"
+              
+            }}
+            autoplay={{ 
+              delay: 3000,
+              disableOnInteraction: false,
+            }}
+            navigation={true}
+            className="slide_box"
+          >
+            <SwiperSlide className='swiperSlide'>
+              <img src={bannerImg6} alt="" />
+              <div className="slide_text">
+                <p>한끼에 담은 오늘의 </p>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide className='swiperSlide'>
+              <img src={bannerImg7} alt="" />
+              <div className="slide_text" >
+                <p>가장 풍요롭고 생생한 시간</p>
+              </div>
+            </SwiperSlide>
+            <div className='swiper_pagination'></div>
+          </Swiper>
+          <div className='baneer_right'>
+            <img src={bannerImg3} alt="" />
+            <img src={bannerImg4} alt="" />
+            <img src={bannerImg5} alt="" />
+            <img src={bannerImg8} alt="" />
+          </div>
+        </div>
     </div>
   )
 }
